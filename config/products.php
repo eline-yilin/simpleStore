19501d218952501f54e3560877c655444eea7fbc
AndyZheng <tianhua.zheng@theoutnet.cn>
2014-12-03 15:48:09 +0800

    products

---
+++ config/products.php
@@ -0,0 +1,21 @@
+<?php
+$str_products = '[
+		{
+				"name":"abc",
+				"price":"25.00",
+				"address":"宁夏",
+				"tag":"养生"
+		
+		},
+		{
+		"name":"def",
+		"price":"46.00",
+		"address":"宁夏",
+		"tag":"滋补"
+
+		}
+]';
+$_products = json_decode($str_products, true);
+?>
+
+
